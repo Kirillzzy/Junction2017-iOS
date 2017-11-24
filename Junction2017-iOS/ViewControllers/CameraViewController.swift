0fef86e7c95b655e29b192cad60b080a3700711c
Kirill <kirillzzy@gmail.com>
2017-11-24 20:10:19 +0200

    Added viewControllers & tab bar

---
+++ Junction2017-iOS/ViewControllers/CameraViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraViewController.swift
+//  Junction2017-iOS
+//
+//  Created by Kirill Averyanov on 24/11/2017.
+//  Copyright © 2017 Kirill Averyanov. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
