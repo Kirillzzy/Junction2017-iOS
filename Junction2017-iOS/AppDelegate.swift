303f7603cf05845deafa3973e79aa583b78fa580
Kirill <kirillzzy@gmail.com>
2017-11-24 18:34:10 +0200

    Added xcode project

---
+++ Junction2017-iOS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Junction2017-iOS
+//
+//  Created by Kirill Averyanov on 24/11/2017.
+//  Copyright © 2017 Kirill Averyanov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
---
+++ Junction2017-iOS/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Junction2017-iOS
+//
+//  Created by Kirill Averyanov on 24/11/2017.
+//  Copyright © 2017 Kirill Averyanov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
