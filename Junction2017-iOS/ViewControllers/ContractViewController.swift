7228feee446848b846433e60a592fffbc9b758f7
Kirill <kirillzzy@gmail.com>
2017-11-25 17:16:56 +0200

    Small code style fixes

--- Junction2017-iOS/ViewControllers/ContractViewController.swift
+++ Junction2017-iOS/ViewControllers/ContractViewController.swift
@@ -11,26 +11,22 @@ import WebKit
 
 class ContractViewController: UIViewController {
   @IBOutlet var mainLabel: UILabel!
-  
   var managedWebView: WKWebView?
   var scannedQrString: String!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     mainLabel.text = scannedQrString
-    
     managedWebView = WKWebView()
     guard let managedWebView = managedWebView else {
-      print("unable to init web view for web3")
+      print("Unable to init web view for web3")
       return
     }
-    
     managedWebView.navigationDelegate = self
     view.addSubview(managedWebView)
-    
     guard let htmlFile = Bundle.main.path(forResource: "web3", ofType: "html"),
       let html = try? String(contentsOfFile: htmlFile, encoding: String.Encoding.utf8) else {
-      print("unable to load web3 html file")
+      print("Unable to load web3 html file")
       return
     }
     managedWebView.loadHTMLString(html, baseURL: Bundle.main.bundleURL)
@@ -39,10 +35,9 @@ class ContractViewController: UIViewController {
 
 extension ContractViewController: WKNavigationDelegate {
   func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-    print("error during navigation: \(error)")
+    print("Error during navigation: \(error)")
   }
-  
   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    print("completed")
+    print("Completed")
   }
 }
